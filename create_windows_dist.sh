fe6774c6527891247741f79924c6e955dc131012
Craig Prevallet <penguintx@hotmail.com>
2025-12-16 15:16:20 -0600

    Update Windows distribution script.

--- create_windows_dist.sh
+++ create_windows_dist.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# Create distributable directory.  Can be anywhere - but insure consistency in subsequent commands.
-mkdir -p /c/Users/barfy/Documents/siliconsneaker2
-# Uncomment this to completely rebuild dist directory
-# cp /mingw64 /c/Users/barfy/Documents/siliconsneaker2/
-cp /home/penguin/siliconsneaker2/target/debug/siliconsneaker2.exe /c/Users/barfy/Documents/siliconsneaker2/bin/
+# cargo build --release
+mkdir -p /c/Users/barfy/Documents/siliconsneaker2/bin/
+cp /home/penguin/siliconsneaker2/target/release/siliconsneaker2.exe /c/Users/barfy/Documents/siliconsneaker2_for_windows/bin/
+
 
